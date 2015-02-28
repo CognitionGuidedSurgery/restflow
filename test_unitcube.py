9004f64fd55ae4aaef0f16502fa237d1ef08b40f
Alexander Weigl <spamling@web.de>
2015-03-01 00:34:04 +0100

    testscript in root because error, dockerignore for smaller images

---
+++ test_unitcube.py
@@ -0,0 +1,77 @@
+#
+# Copyright (C) 2013-2014 Alexander Weigl, Nicolai Schoch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/>.
+
+"""Local test case with unitcube
+
+"""
+
+__author__ = "Alexander Weigl uiduw@student.kit.edu>"
+__date__ = "2014-08-27"
+__version__ = "0.1"
+
+
+from restflow import  hf3binding, hf3configs
+from restflow.base import read_xml
+from path import path
+
+
+assets_dir = path("share/unitcube")
+
+
+##
+# Create a session in working
+session = hf3binding.Hiflow3Session(path("tmp/"))
+
+##
+# Read in HF3 and BCdata configs from share/unitcube
+session.hf3 = read_xml(assets_dir/"hf3_000.xml")
+session.bc =  read_xml(assets_dir/"bc_000.xml")
+
+##
+# Fix the path to the unitcube.vtu
+# This is relative to asset_dir, but the working dir has changed
+mesh = (assets_dir/"unitcube.vtu").abspath()
+update = hf3binding.update_meshfile(mesh)
+
+# An update is a dictionary with new values
+print update
+
+# Apply on the session
+session.update_hf3(update)
+
+# Everything should be alright
+# We start elasticity
+
+print session.run()
+# New constraints for the next run
+session.bc =  read_xml(assets_dir/"bc_001.xml")
+
+# start again, the mesh from step 25 should be taken
+print session.run()
+
+
+# renaming files for paraview
+import restflow.vtkfunctions
+files = sorted(session.get_result_files())
+output = session.working_dir / "output"
+output.makedirs_p()
+
+for i, f in enumerate(files):
+    restflow.vtkfunctions.write_vtu(
+        restflow.vtkfunctions.read_ugrid(f),
+        output/"mesh_%04d.vtu" % i)
+
+
